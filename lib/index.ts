4a1036d6ae128a16f026879808347acb9b2c3811
Fahleh <eddyukpong@gmail.com>
2026-08-15 14:48:31 +0100

    revert: restore lib/errorHandler.ts
    Mistakenly deleted as dead code. This file is kept deliberately as a reference artifact. See architecture.md

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 export { AsyncQueue } from "./asyncQueue";
 export { createCache } from "./createCache";
 export { fetcher } from "./fetcher";
+export { handleError } from "./errorHandler";
 export { createStore } from "./createStore";
 export { createProject, createTask } from "./entityFactory";
 export { updateProject, updateProjectStatus, updateTask, updateTaskStatus  } from "./updateImmutable";
